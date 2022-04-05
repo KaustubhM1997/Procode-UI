2381aeb7b37b38702c1e5050ac5b276d5e26ac1b
Kaustubh Manglurkar <46368989+KaustubhM1997@users.noreply.github.com>
2022-04-05 19:19:49 +0530

    refactored: nav for mobile

---
+++ navigation/nav.js
@@ -0,0 +1,16 @@
+// const hamButton = document.querySelector(".nav-ham-container");
+// const drawerHam = document.querySelector(".drawer-container");
+// const closBtn = document.querySelector(".drawer-close");
+
+
+// hamButton.addEventListener("click",()=>{
+//     drawerHam.classList.add("drawerHam-trans-on");
+//     drawerHam.classList.remove("drawerHam-trans-off");
+// })
+
+// const drawerShut = () => {
+//     drawerHam.classList.remove("drawerHam-trans-on");
+//     drawerHam.classList.add("drawerHam-trans-off");
+// }
+
+// closBtn.addEventListener("click",drawerShut)
