12a871d2286e64af885d90fc8c57e88044d58c48
Kaustubh Manglurkar <46368989+KaustubhM1997@users.noreply.github.com>
2022-04-06 21:49:18 +0530

    feat: functional toast

--- toast/toast.js
+++ toast/toast.js
@@ -1,9 +1,14 @@
-const toastContainer = document.querySelector("#toast-content");
+const toastContainer = document.querySelector(".toast-container");
 
 const toastClose = document.querySelector("#toast-btn");
 
 const closeContainer = () => {
+
+    console.log("hello");
  toastContainer.classList.add("hidden");
+
+ console.log(toastContainer);
+ console.log(toastClose);
 };
 
 toastClose.addEventListener("click", closeContainer);
