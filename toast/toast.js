b1f928fe6a7ee35076bc6e682700e0e88191c2c7
Kaustubh Manglurkar <46368989+KaustubhM1997@users.noreply.github.com>
2022-04-06 16:45:34 +0530

    refactor

--- toast/toast.js
+++ toast/toast.js
@@ -1,6 +1,6 @@
-const toastContainer = document.querySelector(".toast-content");
+const toastContainer = document.querySelector("#toast-content");
 
-const toastClose = document.querySelector(".toast-btn");
+const toastClose = document.querySelector("#toast-btn");
 
 const closeContainer = () => {
  toastContainer.classList.add("hidden");
